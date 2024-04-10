e4495388e3f6e25a46a2ca4043941ebdffdddf72
Felipe <fperezferraro@gmail.com>
2024-04-10 11:49:02 -0300

    Created router provider in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './utils/router';
 
 const App = () => {
-  return (
-    <main></main>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
