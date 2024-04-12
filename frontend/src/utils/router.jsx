360a82d459cf4ff3c5f9fa29aec97535dee13e0a
Felipe <fperezferraro@gmail.com>
2024-04-12 15:27:02 -0300

    added see product route

--- frontend/src/utils/router.jsx
+++ frontend/src/utils/router.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../pages/Layout';
 import Home from '../pages/Home';
 import Shopping from '../pages/Shopping';
+import Product from '../components/shopping/Product';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,14 @@ const router = createBrowserRouter([
     element: (
       <Layout>
         <Shopping />
+      </Layout>
+    ),
+  },
+  {
+    path: '/shopping/product/:id',
+    element: (
+      <Layout>
+        <Product />
       </Layout>
     ),
   },
