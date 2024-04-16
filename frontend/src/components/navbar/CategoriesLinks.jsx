75d958c4814c45d100b548028d282ab8cb34a8d1
Felipe <fperezferraro@gmail.com>
2024-04-16 19:42:03 -0300

    Updated CategoriesLinks component

--- frontend/src/components/navbar/CategoriesLinks.jsx
+++ frontend/src/components/navbar/CategoriesLinks.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useProducts } from '../../hooks/useProducts';
 import { chevronUp, chevronDown } from '../../utils/icons';
-
 import { NavLink } from 'react-router-dom';
 
 const CategoriesLinks = ({ handleToggleMenu }) => {
@@ -13,27 +12,33 @@ const CategoriesLinks = ({ handleToggleMenu }) => {
   };
   return (
     <li
-      className="w-full border-b border-black md:border-0 text-xl font-medium md:hover:text-yellow transition ease md:w-auto md:text-second text-start"
+      className="w-full border-b border-black md:border-0 text-xl font-medium  transition ease md:w-auto md:text-second text-start pl-1 cursor-pointer"
       onClick={handleShowCategories}
     >
       <div className="flex justify-between items-center">
         <p>Shopping</p>
-        {showCategories ? chevronUp : chevronDown}
+        <span className="md:hidden">
+          {showCategories ? chevronUp : chevronDown}
+        </span>
       </div>
       <div
         className={`${
           showCategories ? 'flex' : 'hidden'
-        } flex-col gap-3 pl-5 pt-2 mb-2`}
+        } flex-col gap-3 pl-5 pt-2 mb-2 md:absolute md:top-6 md:bg-green md:w-full md:left-0 md:p-3 md:rounded`}
       >
-        <NavLink to="/shopping" className="text-sm" onClick={handleToggleMenu}>
+        <NavLink
+          to="/shopping"
+          className="text-sm md:hover:text-yellow md:text-end md:text-base"
+          onClick={handleToggleMenu}
+        >
           All Products
         </NavLink>
         {uniqueCategories &&
           uniqueCategories.map((category) => (
             <NavLink
               key={category}
-              to={`/shopping/${category}`}
-              className="text-sm"
+              to={`/${category}`}
+              className="text-sm md:hover:text-yellow md:text-end md:text-base"
               onClick={handleToggleMenu}
             >
               {category}
