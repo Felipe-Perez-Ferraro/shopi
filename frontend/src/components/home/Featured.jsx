90248466eefabe1c06be40b3ba6db0ea950fbb57
Felipe <fperezferraro@gmail.com>
2024-04-16 19:41:03 -0300

    Updated Featured component

--- frontend/src/components/home/Featured.jsx
+++ frontend/src/components/home/Featured.jsx
@@ -1,8 +1,8 @@
 import FeaturedProducts from './FeaturedProducts';
 import Slider from 'react-slick';
 import { useSlider } from '../slider/SliderFuncs';
-import { spinner } from '../../utils/icons';
 import { useProducts } from '../../hooks/useProducts';
+import Loading from '../slider/Loading';
 
 const Featured = () => {
   const { settings } = useSlider();
@@ -13,14 +13,7 @@ const Featured = () => {
       <section className="relative max-w-xs mx-auto md:max-w-2xl lg:max-w-4xl xl:max-w-7xl">
         <h2 className="font-bold text-2xl">Featured Products</h2>
         {loading ? (
-          <article className="flex justify-center pt-10">
-            <div className="bg-gray py-8 px-14 rounded-md flex flex-col gap-4">
-              <svg className="animate-spin text-primary" viewBox="0 0 24 24">
-                {spinner}
-              </svg>
-              <p className="text-primary text-xl font-bold">Loading</p>
-            </div>
-          </article>
+          <Loading />
         ) : (
           <div>
             <Slider {...settings}>
