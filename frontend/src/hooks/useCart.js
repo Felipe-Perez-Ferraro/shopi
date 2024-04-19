902900659d1eddd7270e0bd3323fb59bd0f74e6e
Felipe <fperezferraro@gmail.com>
2024-04-19 14:36:59 -0300

    Updated usecart custom hook

--- frontend/src/hooks/useCart.js
+++ frontend/src/hooks/useCart.js
@@ -2,7 +2,47 @@ import { useContext } from 'react';
 import { CartContext } from '../context/cart';
 
 export function useCart() {
-  const cart = useContext(CartContext);
+  const { cart, setCart } = useContext(CartContext);
 
-  return cart;
+  const addToCart = (product) => {
+    setCart((prev) => [
+      ...prev,
+      {
+        ...product,
+        cartQuantity: 1,
+      },
+    ]);
+  };
+
+  const totalCart = cart.reduce(
+    (acc, curr) => acc + curr.price * curr.cartQuantity,
+    0
+  );
+
+  const deleteItem = (id) => {
+    const newCart = cart.filter((item) => item._id !== id);
+    setCart(newCart);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const addQty = (product) => {
+    const productIndex = cart.findIndex((item) => item._id === product._id);
+
+    const newCart = [...cart];
+    newCart[productIndex].cartQuantity += 1;
+    setCart(newCart);
+  };
+
+  const subQty = (product) => {
+    const productIndex = cart.findIndex((item) => item._id === product._id);
+
+    const newCart = [...cart];
+    newCart[productIndex].cartQuantity -= 1;
+    setCart(newCart);
+  };
+
+  return { cart, addToCart, totalCart, deleteItem, clearCart, addQty, subQty };
 }
